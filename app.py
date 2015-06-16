2c94ff94fc6b9d6d113c8978f2c9f1d9af6d8172
Alexey Cherepanov <avcherepanov@gmail.com>
2015-06-16 15:52:35 +0300

    Use 4 spaces for indentation now

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return '<h1>Per aspera ad astra</h1>'
+    return '<h1>Per aspera ad astra</h1>'
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
